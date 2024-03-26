841fbd8e88b1a7b9ea342263999aba7f1725ca5d
Hyosang Kang <hyosang@dgist.ac.kr>
2024-03-26 12:05:42 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"em2024-hkang/cal"
 	"em2024-hkang/game"
-	"fmt"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
-	fmt.Println(cal.Exp(1))
 	ebiten.SetWindowSize(600, 600)
 	g := game.Game{
 		F: cal.Function{
@@ -22,6 +20,5 @@ func main() {
 		Yb: [2]float64{.9, 3.1},
 		N:  20,
 	}
-	fmt.Println(cal.Exp(2))
 	ebiten.RunGame(&g)
 }
